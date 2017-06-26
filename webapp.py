701daf1d566a7b9622c5c35bc4ecf7532debf8f3
sky-adams <skyadams@sbunified.org>
2017-06-26 16:54:51 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,10 +1,10 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, render_template
 
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
 @app.route("/")
 def render_main():
-    return app.send_static_file('home.html')
+    return render_template('home.html')
     
 if __name__=="__main__":
     app.run(debug=False, port=54321)
