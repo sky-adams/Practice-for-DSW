f17385acb43424847b08e95e255cb5015caa6e88
sky-adams <skyadams@sbunified.org>
2017-06-27 14:18:41 -0700

    Update scripts.js
    Added function for create account button

--- static/scripts.js
+++ static/scripts.js
@@ -1,1 +1,10 @@
+$(document).ready(function(){
 
+   //function that executes when submit button for creating an account is clicked
+   $("#create").click(function(){
+      if ($("#pswd1").text() != $("#pswd2").text()){
+        $("#error").text("Passwords do not match.")
+      }
+   });
+
+});
