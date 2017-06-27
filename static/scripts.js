92fd103537389a4fb429406088d96b808996ef03
sky-adams <skyadams@sbunified.org>
2017-06-27 14:31:42 -0700

    Update scripts.js
    Changed access of passwords

--- static/scripts.js
+++ static/scripts.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
    //function that executes when submit button for creating an account is clicked
    $("#create").click(function(){
-      if ($("#pswd1").text() != $("#pswd2").text()){
+      if ($("#pswd1").val() != $("#pswd2").val()){
         $("#error").text("Passwords do not match.")
       }
    });
