20a889481a00915cecacbc6b0cd3e47136e2d969
sky-adams <skyadams@sbunified.org>
2017-06-28 08:05:30 -0700

    Update scripts.js

--- static/scripts.js
+++ static/scripts.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 
    //function that executes when submit button for creating an account is clicked
+   //can be a void function, but returning false prevents the action set in the html form tag from executing
    $("#createaccount").submit(function(){
       if ($("#pswd1").val() != $("#pswd2").val()){
         $("#error").text("Passwords do not match.")
